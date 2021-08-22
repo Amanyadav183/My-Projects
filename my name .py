056b5b5fccfca3a240ba0974bdd53a7d23861d0f
Amanyadav183 <72963272+Amanyadav183@users.noreply.github.com>
2021-08-22 23:16:07 +0530

    Add files via upload

---
+++ my name .py
@@ -0,0 +1,210 @@
+from turtle import *
+color("red")
+pensize(10)
+speed(3)
+bgcolor("black")
+
+
+######for A
+left(120)
+forward(100)
+left(120)
+forward(100)
+left(180)
+forward(45)
+right(60)
+forward(55)
+
+
+######for space
+for i in range(1):
+    color("black")
+    pensize(0)
+    forward(60)
+
+##### for M    
+for j in range(1):
+    color("red")
+    pensize(10)
+    right(90)
+    forward(40)
+    right(180)
+    forward(90)
+    right(135)
+    forward(50)
+    left(90)
+    forward(50)
+    right(135)
+    forward(90)
+
+
+#######for space
+for k in range(1):
+    color("black")
+    pensize(0)
+    left(90)
+    forward(140)
+
+
+######for A
+for r in range(1):
+    color("red")
+    pensize(10)
+    left(120)
+    forward(100)
+    left(120)
+    forward(100)
+    left(180)
+    forward(45)
+    right(60)
+    forward(55)
+
+
+
+
+#######for space
+for t in range(1):
+    color("black")
+    pensize(0)
+    forward(60)
+
+
+
+######for N
+for f in range(1):
+    color("red")
+    pensize(10)
+    right(90)
+    forward(40)
+    right(180)
+    forward(90)
+    right(150)
+    forward(100)
+    right(210)
+    forward(90)
+
+
+########for space
+for s in range(1):
+    color("black")
+    pensize(0)
+    right(90)
+    forward(60)
+    right(90)
+    forward(120)
+    right(90)
+    forward(540)
+
+
+
+######for Y
+    for q in range(1):
+        color("red")
+        pensize(10)
+        left(120)
+        forward(60)
+        left(120)
+        forward(60)
+        left(180)
+        forward(60)
+        left(30)
+        forward(45)
+
+
+
+
+########for space
+for h in range(1):
+    color("black")
+    pensize(0)
+    left(90)
+    forward(150)
+
+
+
+######for A
+
+for o in range(1):
+    color("red")
+    pensize(10)
+    left(120)
+    forward(100)
+    left(120)
+    forward(100)
+    left(180)
+    forward(45)
+    right(60)
+    forward(55)
+
+
+
+######for space
+for m in range(1):
+    color("black")
+    pensize(0)
+    forward(60)
+
+
+#####for D
+    for d in range(1):
+        color("red")
+        pensize(10)
+        right(90)
+        forward(45)
+        left(180)
+        forward(90)
+        right(150)
+        forward(55)
+        right(60)
+        forward(55)
+
+
+#####for space
+for z in range(1):
+    color("black")
+    pensize(0)
+    left(120)
+    forward(150)
+
+
+
+
+########for A
+    for a in range(1):
+        color("red")
+        pensize(10)
+        left(120)
+        forward(100)
+        left(120)
+        forward(100)
+        left(180)
+        forward(45)
+        right(60)
+        forward(55)
+
+
+
+#######for space
+for c in range(1):
+    color("black")
+    pensize(0)
+    left(90)
+    forward(45)
+    right(90)
+    forward(160)
+
+
+
+
+#####for V
+for v in range(1):
+    color("red")
+    pensize(10)
+    right(120)
+    forward(100)
+    right(120)
+    forward(100)
+    right(180)
+    forward(45)
+  
+done()
